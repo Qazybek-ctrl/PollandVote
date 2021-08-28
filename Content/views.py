f94a6118a665b7d8c478bb5464906c4a88b53d1a
Qazybek-ctrl <allinmuggles@yahoo.com>
2021-08-28 23:08:38 +0600

    Added static files and 3 new pages

--- Content/views.py
+++ Content/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'Content/index.html')
+
+def blog(request):
+    return render(request, "Content/blog.html")
--- Project1/settings.py
+++ Project1/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,4 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-cdn')
